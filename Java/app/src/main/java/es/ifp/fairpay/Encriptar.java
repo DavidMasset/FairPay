42221f11ebec7307f9c5e4b5d12127eb85bd9fef
Julio Damas <jkpio27@gmail.com>
2025-08-03 17:04:10 +0200

    Refactorizar la clase Encriptar
    - Se renombran los métodos `Crypt` y `Verificar` a `encriptarHash` y `verificarHash` respectivamente.
    - Se añaden validaciones de entrada en `encriptarString` para evitar cadenas nulas o vacías.
    - Se implementa manejo de excepciones en `verificarHash` para capturar errores durante la verificación.
    - Se actualiza la documentación Javadoc para reflejar los cambios y mejorar la claridad.

--- Java/app/src/main/java/es/ifp/fairpay/Encriptar.java
+++ Java/app/src/main/java/es/ifp/fairpay/Encriptar.java
@@ -3,18 +3,35 @@ package es.ifp.fairpay;
 import at.favre.lib.crypto.bcrypt.BCrypt;
 public class Encriptar {
 
-    // Metodo para encriptar una string con las opciones por defecto
-    // Devuelve el HashString necesario para la verificacion
-    public String Crypt(String str) {
+    /**
+     * Encripta una cadena de texto utilizando el algoritmo BCrypt con la configuración por defecto.
+     *
+     * @param str La cadena de texto a encriptar. No puede ser nula ni vacía.
+     * @return El hash BCrypt de la cadena de texto. Este hash es necesario para la verificación posterior.
+     * @throws IllegalArgumentException si la cadena de entrada es nula o vacía.
+     */
+    public static String encriptarHash(String str) {
+        if (str == null || str.isEmpty()){
+            throw new IllegalArgumentException("La cadena no puede ser nula o vacía");
+        }
         return BCrypt.withDefaults().hashToString(12, str.toCharArray());
     }
 
-    // Metodo para verificar si una string se corresponde con su HashString
-    public boolean Verificar(String str, String bcryptHashString) {
-
-        BCrypt.Result result = BCrypt.verifyer().verify(str.toCharArray(), bcryptHashString);
-
-        return result.verified;
+    /**
+     * Verifica si una cadena de texto coincide con un hash BCrypt.
+     *
+     * @param str La cadena de texto a verificar.
+     * @param bcryptHashString El hash BCrypt con el que se comparará la cadena.
+     * @return {@code true} si la cadena coincide con el hash, {@code false} en caso contrario o si ocurre un error durante la verificación.
+     */
+    public static boolean verificarHash(String str, String bcryptHashString) {
+        try {
+            BCrypt.Result result = BCrypt.verifyer().verify(str.toCharArray(), bcryptHashString);
+            return result.verified;
+        } catch(Exception e) {
+            System.out.println("Error al verificar el hash: " + e.getMessage());
+            return false;
+        }
     }
 
 }
