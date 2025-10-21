ee76de900a1f8221d149383f3c10a1a674b7bb29
David Masset Rojas <dmassetr@student.ifp.es>
2025-10-21 19:10:16 +0200

    Fragment Wallet con navegación

--- Java/app/src/main/java/es/ifp/fairpay/activities/InicioActivity.java
+++ Java/app/src/main/java/es/ifp/fairpay/activities/InicioActivity.java
@@ -10,7 +10,6 @@ import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import es.ifp.fairpay.R;
 
 
--- Java/app/src/main/java/es/ifp/fairpay/fragments/WalletFragment.java
+++ Java/app/src/main/java/es/ifp/fairpay/fragments/WalletFragment.java
@@ -1,61 +1,35 @@
 package es.ifp.fairpay.fragments;
 
+
+
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
 
 import es.ifp.fairpay.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link WalletFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class WalletFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public WalletFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment WalletFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static WalletFragment newInstance(String param1, String param2) {
-        WalletFragment fragment = new WalletFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    NavController controladorNavegacion;
+    protected ListView lista;
+    protected ArrayList<String> listaArray = new ArrayList<String>();
+    protected ArrayAdapter<String> adaptador;
+    protected Button verMovimientos;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -63,4 +37,37 @@ public class WalletFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_wallet, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //Instancia de los elementos
+        lista = view.findViewById(R.id.listView_movimientos_wallet);
+        verMovimientos = view.findViewById(R.id.boton_vertodo_wallet);
+        controladorNavegacion = Navigation.findNavController(view);
+
+        // Contenido de prueba, se carga desde BD
+        listaArray.clear();
+        listaArray.add("Usuario1");
+        listaArray.add("Usuario2");
+        listaArray.add("Usuario3");
+        listaArray.add("Usuario4");
+        listaArray.add("Usuario5");
+
+        adaptador = new ArrayAdapter<String>(getContext(),android.R.layout.simple_list_item_1 , listaArray);
+        lista.setAdapter(adaptador);
+
+        verMovimientos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //controladorNavegacion.navigate(R.id.movimientosFragment, null);
+                BottomNavigationView bottomNav = getActivity().findViewById(R.id.bottom_nav);
+                bottomNav.setSelectedItemId(R.id.movimientosFragment);
+            }
+        });
+
+
+    }
+
 }
